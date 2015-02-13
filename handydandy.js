ee8cf4ea02b80ef3ee6e475af38b885242781cc4
ramie deihl <ramie@idealorganics.com>
2015-02-13 08:43:04 -0500

    1 st script
    added one-time refresh

---
+++ handydandy.js
@@ -0,0 +1,8 @@
+//One time page refresh after first page load
+window.onload = function() {
+    if(!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+    }
+}
+// ------------------------------------------
